6d4bfcc0af967c86357a8d2004a99ec677be51b2
codnjs779 <codnjs779@gmail.com>
2021-11-22 23:09:40 +0900

    metatag

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,9 @@
 import styled from "styled-components";
-import { Helmet } from "react-helmet";
 import Rootrouter from "../routers/Rootrouter";
-import editHeart from "../img/editheart.png";
 
 function App() {
-    const currentUrl = "https://chemistry-test.co.kr";
     return (
         <>
-            <Helmet>
-                <meta property="og:url" content={currentUrl} />
-                <meta property="og:title" content="우리의 모든 것" />
-                <meta property="og:description" content="mbti 별자리 띠 혈액형 모든 궁합을 한번에 확인해보세요!" />
-                <meta property="og:image" content={editHeart} />
-            </Helmet>
             <BasicStyle>
                 <Rootrouter />
             </BasicStyle>
