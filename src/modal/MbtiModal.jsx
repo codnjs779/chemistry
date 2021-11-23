1af9eb021b35471fb1b2d65fc0a12168844fa33c
codnjs779 <codnjs779@gmail.com>
2021-11-23 21:02:43 +0900

    style edit

--- src/modal/MbtiModal.jsx
+++ src/modal/MbtiModal.jsx
@@ -178,7 +178,7 @@ const MbtiModal = (props) => {
 
 const MbtiModalStyle = styled.div`
     .MbtiModal {
-        width: 400px;
+        width: 380px;
         height: 100%;
         background-color: rgb(255 255 255);
         border-radius: 35px;
