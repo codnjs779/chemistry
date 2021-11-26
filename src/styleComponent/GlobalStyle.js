06084742091ef87e97b3f2860ae539197c3d2c86
codnjs779 <codnjs779@gmail.com>
2021-11-26 15:10:52 +0900

    fix

--- src/styleComponent/GlobalStyle.js
+++ src/styleComponent/GlobalStyle.js
@@ -6,7 +6,7 @@ export const GlobalStyle = styled.div`
     background-image: url(${check});
     width: 400px;
     height: 100%;
-    padding-bottom: 50%;
+    padding-bottom: 100%;
     margin: auto;
     font-family: "MaruBuri";
 `;
